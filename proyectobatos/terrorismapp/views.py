a43d94a035e3dc3a2d88d65fd26ff5011a457b3f
NicoBrandstetterr <nicolas.brandstetter@hotmail.com>
2022-06-24 12:00:17 -0400

    hola

--- proyectobatos/terrorismapp/views.py
+++ proyectobatos/terrorismapp/views.py
@@ -7,8 +7,8 @@ def seleccion_preguntas(request):
 
 
 def primeraQuery(request):
-    #query=gName.objects.raw('SELECT * FROM terrorismapp_gname WHERE gname = Hutu extremists')
-    query=Gname.objects.filter(gname='Hutu extremists')
+    query=Gname.objects.raw("SELECT * FROM terrorismapp_gname WHERE gname = 'Hutu extremists'")
+    #query=Gname.objects.filter(gname='Hutu extremists')
     return render(request, "1query.html",{"Tabla":query})
 
 
@@ -16,6 +16,7 @@ def segundaQuery(request):
     #query=gName.objects.raw('SELECT * FROM Tabla2 WHERE gname = "Hutu extremists"')
     query=Gname.objects.filter(gname='Hutu extremists')
     return render(request, "2query.html",{"Tabla2":query})
+
 
 def terceraQuery(request):
     #query=gName.objects.raw('SELECT * FROM Tabla3 WHERE gname = "Hutu extremists"')
